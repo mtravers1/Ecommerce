af6c8ed7f9bf1cfc9782180344f99b39ffc12295
Michael Travers <michael.travers@udc.edu>
2024-09-03 21:06:06 -0400

    working on nav menu

---
+++ scripts/nav.js
@@ -0,0 +1,16 @@
+let nav=document.querySelector('#nav')
+
+nav.addEventListener('click', menu)
+
+const menu = ()=>{
+    console.log('clicked')
+    // let n= document.querySelector('#menu')
+    // n.innerHTML=
+    // `
+    // <div>
+    // <p>
+
+    // </div>
+    
+    // `
+}
